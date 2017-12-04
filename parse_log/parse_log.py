642f0e3a447dc4aaf65652817c2e9c11bc372e7e
Beesmurfy <beesmurfy@gmail.com>
2017-12-04 14:13:24 +0200

    working on new samples for log

--- parse_log/parse_log.py
+++ parse_log/parse_log.py
@@ -5,30 +5,41 @@ import pprint
 import shutil
 import os
 
-voco_data_base = "/home/bartek/Projects/ASR/voco_data/"
+# voco_data_base = "/home/bartek/Projects/ASR/voco_data/"
+
+voco_data_base = "/Users/bartek/other/voco_data/"
 audio_data_directory = voco_data_base + "audio_data/"
 audio_records_directory = voco_data_base + "audio_records/"
 
-# 2017-10-25 22:02:47.259551,
-# LIVE00000062_00000,
-# hotel,
-# /usr/bin/xdotool key h,
-# 0.190190076828,
-# /home/bartek/Projects/ASR/voco_data/staging/LIVE00000062_00000.wav
+ROOT = ["/home/bartek/Projects/ASR", "/home/bartek/Projects/ASR/"]
 
+#----------------------------------------------------------------------------
+# Open Parse Counter
+#----------------------------------------------------------------------------
 try:
     with open("parse_counter.txt") as f:
         parse_counter = int(f.read())
 except IOError:
     parse_counter = 0
 
+#----------------------------------------------------------------------------
+# Open Naming List Counter
+#----------------------------------------------------------------------------
 try:
     with open(audio_records_directory + "naming_list_counter.txt") as f:
         naming_list_counter = int(f.read())
 except IOError:
     naming_list_counter = 0
 
-shutil.copy2("log","log.backup")
+#----------------------------------------------------------------------------
+# Backup Log File
+#----------------------------------------------------------------------------
+
+shutil.copy2("log", "log.backup")
+
+#----------------------------------------------------------------------------
+# Iterate over log file
+#----------------------------------------------------------------------------
 
 log_file = open('log', 'r')
 log = log_file.readlines()
@@ -48,6 +59,7 @@ for x in log:
 
     # if phrase.find("eight") != -1:
     #     phrase = phrase.replace("eight","space")
+
     if True:
         print("---------------------------")
         print(UID + "\n")
@@ -57,30 +69,43 @@ for x in log:
 
         processed = False
         while not processed:
-            os.system("aplay " + audio_sample_file_path)
+
+            # os.system("aplay " + audio_sample_file_path)
+
+            os.system("afplay " + audio_sample_file_path.replace(
+                "/home/bartek/Projects/ASR/voco_data/",
+                "/Users/bartek/other/voco_data/"))
 
             accept = raw_input("accept?")
+
             if accept == "n":
                 with open("error_log", "a") as f:
                     f.write(x)
                 print("Rejected!")
                 processed = True
+
             elif accept == "r":
                 processed = False
-            else:
 
-                shutil.move(audio_sample_file_path,
-                            audio_data_directory + UID + ".wav")
+            else:
+                shutil.move(
+                    audio_sample_file_path.replace(
+                        "/home/bartek/Projects/ASR/voco_data/",
+                        "/Users/bartek/other/voco_data/"),
+                    audio_data_directory + UID + ".wav")
 
                 outputfile = open(audio_records_directory + 'wav.scp', 'a')
-                outputfile.write(UID + " " + audio_sample_file_path + "\n")
+                outputfile.write(UID + " " + "ROOT/voco_data/audio_data/" + UID
+                                 + ".wav" + "\n")
 
                 outputfile = open(audio_records_directory + "text", 'a')
                 outputfile.write(UID + " " + phrase + "\n")
 
                 outputfile = open(audio_records_directory + 'utt2spk', 'a')
                 outputfile.write(UID + " bartek" + "\n")
+
                 naming_list_counter += 1
+
                 with open(audio_records_directory + "naming_list_counter.txt",
                           "w") as f:
                     f.write(str(naming_list_counter))
